4ecff20105f285ab77431a8e852e66731ed9a76d
Hussein <hussein.h.tech@gmail.com>
2021-09-07 11:33:02 +0300

    add env variables to firebase

--- config/firebase.js
+++ config/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseApp = initializeApp({
-  apiKey: "AIzaSyAj4G2799xjHm5svOlrwp-6jTvIl2agP_E",
-  authDomain: "medicine-abfff.firebaseapp.com",
-  projectId: "medicine-abfff",
-  storageBucket: "medicine-abfff.appspot.com",
-  messagingSenderId: "783550517561",
-  appId: "1:783550517561:web:1b376c303334c0c21d58e8",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 });
 export default firebaseApp;
 
--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  env: {
+    apiKey: "AIzaSyAj4G2799xjHm5svOlrwp-6jTvIl2agP_E",
+    authDomain: "medicine-abfff.firebaseapp.com",
+    projectId: "medicine-abfff",
+    storageBucket: "medicine-abfff.appspot.com",
+    messagingSenderId: "783550517561",
+    appId: "1:783550517561:web:1b376c303334c0c21d58e8",
+  },
+};
